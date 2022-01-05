2258fab530a023b577ccdcd118b070a87b482954
stephanie <stephanie.menu.cda@gmail.com>
2022-01-05 16:53:55 +0100

    avancement

--- contact.php
+++ contact.php
@@ -13,7 +13,7 @@
             
         <input type="text" name = "name" placeholder="Entrer votre Nom">
         <input type="text" name = "prenom" placeholder="Entrer votre Prénom">
-        <input type="email" name = "mail" placeholder="Entrer votre Email">
+        <input type="mail" name = "mail" placeholder="Entrer votre Email">
         <input type="tel" name = "telephone" placeholder="Entrer votre Téléphone">
         <input type="text" name = "adresse" placeholder="Entrer votre Adresse">
         <input class="submit" type="submit" value = "Envoyer">
@@ -21,8 +21,8 @@
     </form>
 </body>
 </html>
-  
-  
+
+
   <!-- // je recupere mes données// -->
 <?php
 $name = $_POST ['name'];
@@ -39,10 +39,68 @@ print_r($tableau); // j'affiche mon tableau//
 // lorsque j ouvre mon fichier php j obtiens un tableau vide //
 ?>
 <?php
+function validatePhoneNumber($string) // creation fonction pour valider le tel
+{
+    $phoneNumberArray =str_split($string); // creation d'un tableau avec str_splice pour separer les chiffres//
+    if ($phoneNumberArray[0] !=0) {// si le premier chiffre different (!=) de 0 alors
+        return false;
+    }
+    foreach($phoneNumberArray as $value){ // 
+        if( !is_numeric($value)){// verif que chaque caractere = numerique
+        return false;
+    }
+    }
+    if (strlen($string) != 10){ // verif taille du numero
+        return false;
+    }
+    return true;
+}
+?>
+<?php
 
-if(empty($name) + empty($prenom) + empty($email) || empty($telephone) || empty($adresse)){
+if(empty($name) || empty($prenom) || empty($email) || empty($telephone) || empty($adresse)){
 echo "champs manquants";
+}else{
+    print_r($tableau);
+}
+?>
+<?php
+// le nom  doit contenir au moins 3 caracteres et pas de chiffre//
+if(strlen($_POST['name']) <=3) // strlen =taille de la chaine raccourcie de string lenght//
+{ echo "Votre nom doit posséder au moins 3 caractères.";
+}
+elseif(!ctype_alpha($name))
+{ 
+echo "Votre nom ne peut pas contenir de chiffres."; }
+?>
+<?php
+// le prenom  doit contenir au moins 3 caracteres et pas de chiffre//
+if(strlen($_POST['prenom']) <=3) { 
+    echo "Votre prenom doit posséder au moins 3 caractères.";
+}
+elseif(!ctype_alpha($prenom))
+{
+    echo "votre nom contient des caracteres non autorises";
 }
 
 ?>
+<?php
+//L'email doit être valide (forme: username@exemple.com)//
+$email = "username@exemple.com";
 
+if (filter_var($email, FILTER_VALIDATE_EMAIL)=== false) {
+echo "L'adresse email est invalide elle doit etre sous forme username@exemple.com.";
+}
+?>
+
+
+<?php
+//-Le numéro de téléphone doit être valide (exemple: 0600000000)//
+if (validatePhoneNumber($telephone) === false){
+    echo "Le numero de téléphone ne doit contenir que des chiffres";
+    }
+
+    ?> 
+    <?php 
+    //Afficher les données sous forme de tableau //
+    
